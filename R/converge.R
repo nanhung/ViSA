2457a6b4811d01d2d216a0b173b454ebe7ba246d
Nan-Hung Hsieh <nh@nanhung.me>
2018-04-11 17:59:50 -0500

    Update converge.R

--- R/converge.R
+++ R/converge.R
@@ -1,6 +1,6 @@
 converge <- function(data, factors = NULL){
   p <- nrow(data$S)
-
+  
   if (length(factors)==nrow(data$S)){
     X.labels <- factors
   } else if (is.null(factors)){
@@ -8,16 +8,17 @@ converge <- function(data, factors = NULL){
   } else {
     stop("The number of factors is not equal!")
   }
-
+  
   X <- as.data.frame(matrix(nrow = p, ncol = 2))
   row.names(X) <- X.labels
   colnames(X) <- c("first order","total order")
   X[,1] <- data$S$`max. c.i.` - data$S$`min. c.i.`
   X[,2] <- data$T$`max. c.i.` - data$T$`min. c.i.`
-
+  
   cat("\nModel runs: ", nrow(data$X))
   cat("\nConvergence of the sensitivity indices:\n")
-  return(print(X))
   cat("\nMaximum of first order:", row.names(X)[which(X[,1] == max(X[,1]))], max(X[,1]), "\n")
   cat("Maximum of total order:", row.names(X)[which(X[,2] == max(X[,2]))], max(X[,2]), "\n")
+  cat("\n")
+  return(print(X))
 }
