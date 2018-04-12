f8964f995b3f1e500092dc9751348a6316e8d20e
Nan-Hung Hsieh <nh@nanhung.me>
2018-04-12 08:58:33 -0500

    Update convergejansen.R

--- R/convergejansen.R
+++ R/convergejansen.R
@@ -1,16 +1,16 @@
-convergejansen <- function(n, n.factors, X, fun){
+convergejansen <- function(n, n.factors, X, fun, nboot = 100, ...){
   df <- as.data.frame(matrix(nrow = n.factors, ncol=0))
   for (n in seq(n/10, n, n/10)){
     X1 <- X(n)
     X2 <- X(n)
-    x <- soboljansen(model = NULL, X1, X2, nboot = 100) #
+    x <- soboljansen(model = NULL, X1, X2, nboot) #
     y <- fun(x$X)
     t <- tell(x,y)
-    c <- converge(t)
+    c <- converge(t, factors = row.names(t$S))
     df <- cbind(df, c$`first order`)
   }
   names(df)<-seq(n/10, n, n/10)
   row.names(df)<-row.names(c)
-
+  
   return(df)
 }
