3e3a3eae0b71029de91be172a41709b22c9b1f49
Nan-Hung Hsieh <nh@nanhung.me>
2018-04-13 11:38:26 -0500

    Update converge.R

--- R/converge.R
+++ R/converge.R
@@ -1,43 +1,35 @@
-converge <- function(data, factors = NULL){
-  p <- nrow(data$S)
+converge <- function(data){
   
-  if (length(factors)==nrow(data$S)){
-    X.labels <- factors
-  } else if (is.null(factors)){
-    X.labels <- paste("X", 1 : p, sep = "")
-  } else {
-    stop("The number of factors is not equal!")
-  }
+  diff <- as.data.frame(matrix(nrow = nrow(data$S), ncol = 2))
+  row.names(diff) <- row.names(data$S)
+  colnames(diff) <- c("first order","total order")
+  diff[,1] <- data$S$`max. c.i.` - data$S$`min. c.i.`
+  diff[,2] <- data$T$`max. c.i.` - data$T$`min. c.i.`
   
-  X <- as.data.frame(matrix(nrow = p, ncol = 2))
-  row.names(X) <- X.labels
-  colnames(X) <- c("first order","total order")
-  X[,1] <- data$S$`max. c.i.` - data$S$`min. c.i.`
-  X[,2] <- data$T$`max. c.i.` - data$T$`min. c.i.`
+  data$diff <- diff
+  data$S$diff <- data$S$`max. c.i.` - data$S$`min. c.i.`
+  data$T$diff <- data$T$`max. c.i.` - data$T$`min. c.i.`
   
-  cat("\nModel runs: ", nrow(data$X))
-  cat("\nConvergence of the sensitivity indices:\n")
-  cat("\nMaximum of first order:", row.names(X)[which(X[,1] == max(X[,1]))], max(X[,1]), "\n")
-  cat("Maximum of total order:", row.names(X)[which(X[,2] == max(X[,2]))], max(X[,2]), "\n")
-  cat("\n")
-  return(print(X))
-}
-
-plot.converge <- function(x){
   p <- 2
   pch = c(21, 24)
-  n <- nrow(x)
+  n <- nrow(data$S)
   at <- 1 : n
   ylim <- c(0, 1)
   xlim <- c(1, n)
   plot(0, xlim = xlim, ylim = ylim, axes = FALSE,
        xlab = "", ylab = "", type = "n")
-  axis(side = 1, at = at, labels = rownames(x))
+  axis(side = 1, at = at, labels = rownames(data$S))
   axis(side = 2)
-  points(x$`first order`, xlim = c(1, n + 1), ylim = ylim, pch = pch[1])
-  points(x$`total order`, xlim = c(1, n + 1), ylim = ylim, pch = pch[2])
+  points(data$S$diff, xlim = c(1, n + 1), ylim = ylim, pch = pch[1])
+  points(data$T$diff, xlim = c(1, n + 1), ylim = ylim, pch = pch[2])
   abline(0.1, 0, lty = 2)
   abline(0.05, 0, lty = 3)
   box()
+  
   legend(x = "topright", legend = c("main effect", "total effect"), pch = pch)
+  cat("\nConvergence of the sensitivity indices:\n")
+  cat("\nMaximum of first order:", row.names(data$diff)[which(data$diff[,1] == max(data$diff[,1]))], max(data$diff[,1]), "\n")
+  cat("Maximum of total order:", row.names(data$diff)[which(data$diff[,2] == max(data$diff[,2]))], max(data$diff[,2]), "\n")
+  cat("\n")
+  print(data$diff)
 }
