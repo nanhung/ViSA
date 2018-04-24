ff1ee383f1f5c1d7e05aec31e3d6e078a09a8d46
Nan-Hung Hsieh <nh@nanhung.me>
2018-04-23 19:30:39 -0500

    Update rfast99.R

--- R/rfast99.R
+++ R/rfast99.R
@@ -137,3 +137,95 @@ print.rfast99 <- function(x, ...) {
     cat("(empty)\n")
   }
 }
+
+plot.rfast99 <- function(x, ylim = c(0, 1), 
+                             y_col = NULL, y_dim3 = NULL, ...) {
+  if (!is.null(x$y)) {
+    p <- dim(x$a)[3]
+    pch = c(21, 24)
+    
+      nodeplot(x$S, xlim = c(1, p + 1), ylim = ylim, pch = pch[1])
+      nodeplot(x$T, xlim = c(1, p + 1), ylim = ylim, labels = FALSE,
+               pch = pch[2], at = (1:p)+.3, add = TRUE)
+      
+    legend(x = "topright", legend = c("main effect", "total effect"), pch = pch)
+  }
+}
+                
+nodeplot <- function(x, xlim = NULL, ylim = NULL, labels = TRUE,
+                     col = par("col"), pch = 21, bg = "white",
+                     add = FALSE, at = NULL, y_col = NULL, y_dim3 = NULL, ...) {
+  n <- nrow(x)
+  if (is.null(xlim)) {
+    xlim <- c(1, n)
+  }
+  if (is.null(ylim)) {
+    ylim <- c(min(x), max(x))
+  }
+  if (is.null(at)) {
+    at <- 1 : n
+  }
+  if (add) {
+    par(new = TRUE)
+  }
+  
+  # axes
+  
+  plot(0, xlim = xlim, ylim = ylim, axes = FALSE,
+       xlab = "", ylab = "", type = "n", ...)
+  if (class(labels) == "logical") {
+    if (labels) {
+      axis(side = 1, at = at, labels = rownames(x))
+    } else {
+      axis(side = 1, at = at, labels = FALSE, tick = FALSE)
+    }
+  } else if (class(labels) == "character") {
+    axis(side = 1, at = at, labels = labels)
+  }
+  axis(side = 2)
+  box()
+  
+  # bias
+  
+  if ("bias" %in% dimnames(x)[[2]]) {
+    if(is.null(y_col) && is.null(y_dim3)){
+      xx <- x[["original"]] - x[["bias"]]
+    } else if(!is.null(y_col) && is.null(y_dim3)){
+      xx <- x[, "original", y_col] - x[, "bias", y_col]
+    } else if(!is.null(y_col) && !is.null(y_dim3)){
+      xx <- x[, "original", y_col, y_dim3] - x[, "bias", y_col, y_dim3]
+    }
+  } else {
+    if(is.null(y_col) && is.null(y_dim3)){
+      xx <- x[["original"]]
+    } else if(!is.null(y_col) && is.null(y_dim3)){
+      xx <- x[, y_col]
+    } else if(!is.null(y_col) && !is.null(y_dim3)){
+      xx <- x[, y_col, y_dim3]
+    }
+  }
+  
+  # confidence intervals
+  
+  if (("min. c.i." %in% dimnames(x)[[2]]) & "max. c.i." %in% dimnames(x)[[2]]) {
+    if(is.null(y_col) && is.null(y_dim3)){
+      min_ci <- x[["min. c.i."]]
+      max_ci <- x[["max. c.i."]]
+    } else if(!is.null(y_col) && is.null(y_dim3)){
+      min_ci <- x[, "min. c.i.", y_col]
+      max_ci <- x[, "max. c.i.", y_col]
+    } else if(!is.null(y_col) && !is.null(y_dim3)){
+      min_ci <- x[, "min. c.i.", y_col, y_dim3]
+      max_ci <- x[, "max. c.i.", y_col, y_dim3]
+    }
+    for (i in 1 : n) {
+      lines(c(at[i], at[i]), c(min_ci[i], max_ci[i]),
+            col = col)
+    }
+  }
+  
+  # points
+  
+  points(at, xx, col = col, pch = pch, bg = bg)
+}
+                
